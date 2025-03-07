c7b89136a40bb8bb3d2e7295764e7b2a05ceb20b
Omar <ardilaomar848@gmail.com>
2025-03-06 23:16:07 -0500

    votaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,20 +29,19 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="relative flex flex-col justify-center items-center min-h-screen p-6 bg-black text-white overflow-hidden">
+    <div className="relative flex flex-col justify-center items-center min-h-screen p-4 bg-black text-white overflow-hidden">
       <div className="bubble-container absolute top-0 left-0 w-full h-full pointer-events-none z-0"></div>
-      <div className="relative z-10 backdrop-blur-md bg-opacity-50 p-6 rounded-lg">
-        <div className=" flex gap-3 text-center justify-center items-center">
-          <h1 className="text-center font-bold text-7xl mb-8">Bienvenidos</h1>
+      <div className="relative z-10 backdrop-blur-md bg-opacity-50 p-6 rounded-lg w-full max-w-5xl">
+        <div className="flex flex-col sm:flex-row gap-3 text-center justify-center items-center">
+          <h1 className="text-center font-bold text-4xl sm:text-7xl mb-4">Bienvenidos</h1>
           <Welcome />
-
         </div>
-        
-        <div className="flex gap-8 text-center justify-center items-center w-full max-w-5xl">
-          <div className="w-1/2">
+
+        <div className="flex flex-col sm:flex-row gap-4 text-center justify-center items-center w-full">
+          <div className="w-full sm:w-1/2">
             <img src={img1} className="w-full h-auto rounded-lg shadow-lg" alt="Elecciones" />
           </div>
-          <div className="w-1/2 text-[25px] font-semibold leading-relaxed">
+          <div className="w-full sm:w-1/2 text-lg sm:text-2xl font-semibold leading-relaxed">
             <h2>
               Bienvenidos a las elecciones de la Institución Educativa César Pompeyo Mendoza
               Hinojosa 2025. Un proceso democrático, transparente y justo, donde cada voto
@@ -50,14 +49,13 @@ const Home = () => {
             </h2>
           </div>
         </div>
-        <div className="flex gap-2 justify-center text-center items-center w-full mt-8 px-6 py-3 border-2 border-white text-white font-bold text-lg rounded-lg transition duration-300 hover:bg-white hover:text-black " onClick={irAOtraPagina}>
-          <button 
-          >
-            Siguiente
-          </button>
+
+        <div className="flex gap-2 justify-center items-center w-full mt-6 px-6 py-3 border-2 border-white text-white font-bold text-lg rounded-lg transition duration-300 hover:bg-white hover:text-black cursor-pointer" onClick={irAOtraPagina}>
+          <button>Siguiente</button>
           <Go />
         </div>
       </div>
+
       <style>
         {`
           .bubble-container {
